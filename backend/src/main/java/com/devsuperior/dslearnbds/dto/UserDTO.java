f5cf408d3e98c1738a232721a2e6c04122f2208f
Marcos Francisco Picosque <mpicosque@gmail.com>
2021-05-01 19:54:30 -0300

    Custom Notifications content for current user, JPQL query

--- backend/src/main/java/com/devsuperior/dslearnbds/dto/UserDTO.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/dto/UserDTO.java
@@ -6,7 +6,6 @@ import com.devsuperior.dslearnbds.entities.User;
 
 public class UserDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
-
 	
 	private Long id;
 	private String name;
